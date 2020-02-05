6d5498a822bb5bcface2918f97be611749312e53
Marissa Rousseau <marissadavisrousseau@gmail.com>
2020-02-05 10:30:37 -0500

    Update removeThemeColor.js

--- removeThemeColor.js
+++ removeThemeColor.js
@@ -1,12 +1,11 @@
 function removeThemeColor()
 {
-   document.querySelector("[name='theme-color']").remove()
-    var head = document.getElementsByTagName('HEAD')[0];
-    var meta = document.createElement('meta');
-    meta.setAttribute('name', 'theme-color')
-    meta.content = "#000000";
-    head.appendChild(meta);
-
+  document.querySelector("[name='theme-color']").remove()
+  var head = document.getElementsByTagName('HEAD')[0];
+  var meta = document.createElement('meta');
+  meta.setAttribute('name', 'theme-color')
+  meta.content = "#000000";
+  head.appendChild(meta);
 }
 
 removeThemeColor();
